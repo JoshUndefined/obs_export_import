fdf3b4d39f16be174de91d04becf1b0b76bd0916
JoshUndefined <joshundefined.tv@gmail.com>
2025-06-28 14:04:37 -0700

    Add command line arg for scene name

--- test_export.py
+++ test_export.py
@@ -1,8 +1,13 @@
 import asyncio
+import argparse
 from scene_exporter import export_scene
 
+parser = argparse.ArgumentParser("test_export")
+parser.add_argument("scene", help="Scene name to export", type=str)
+args = parser.parse_args()
+
 async def main():
-    await export_scene("KoalatyDono")
+    await export_scene(args.scene)
 
 if __name__ == "__main__":
     asyncio.run(main())
