0d37dafeb0c5709f3f72a3ee47d6bc129e0a38e6
JoshUndefined <joshundefined.tv@gmail.com>
2025-06-28 15:09:22 -0700

    Add description to argument parser

--- src/export_scene.py
+++ src/export_scene.py
@@ -2,7 +2,9 @@ import asyncio
 import argparse
 from lib.scene_exporter import export_scene
 
-parser = argparse.ArgumentParser("export_scene")
+parser = argparse.ArgumentParser(
+    description="Exports an OBS scene to JSON to rebuild in a separate OBS instance/scene collection"
+)
 parser.add_argument("scene", help="Scene name to export", type=str)
 args = parser.parse_args()
 
--- src/import_scene.py
+++ src/import_scene.py
@@ -2,12 +2,13 @@ import asyncio
 import argparse
 from lib.scene_importer import import_scene
 
-parser = argparse.ArgumentParser("import_scene")
+parser = argparse.ArgumentParser(
+    description="Imports a saved OBS scene from JSON and rebuilds it in OBS"
+)
 parser.add_argument("scene", help="Scene name to import", type=str)
 args = parser.parse_args()
 
 async def main():
-    # print(f"output/packaged_scenes/{args.scene}.json")
     await import_scene(f"output/packaged_scenes/{args.scene}.json")
 
 if __name__ == "__main__":
