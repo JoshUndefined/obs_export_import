a023758377dca87eebc25a0e94ce00e94a2c5d5c
JoshUndefined <joshundefined.tv@gmail.com>
2025-06-28 13:52:49 -0700

    Add dotenv

---
+++ main.py
@@ -0,0 +1,7 @@
+# OBS Export/Import
+
+# Configure OBS WS 5.x connection
+
+# Given SceneName, gather the Scene Inputs and dependancies, generate a config file and export
+
+# To import, run OBS WS API and dynamically re-generate the Scene Input
